d44c24a9613d7cc62ab1740a2024f52fafd6bb6b
Pratik Chakraborty <pratik@talkinglands.com>
2022-11-08 12:25:19 +0530

    my firsr release

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+var moment = require('moment'); // require
+
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Hello from Pratik!</h1>");
+});
+
+
+app.get("/api/v1/instagram", (req, res) => {
+    const instaSocial = {
+        userName: "_pratik.c",
+        followers: 301,
+        following: 324,
+        posts: 20,
+        date: new Date()
+    };
+    res.status(200).json(instaSocial)
+    
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+    const facebookSocial = {
+        userName: "Pratik Chakraborty",
+        followers: 2001,
+        following: 2001,
+        posts: 200,
+        date: new Date()
+    };
+    res.status(200).json(facebookSocial)
+    
+});
+
+app.get("/api/v1/linkedin", (req, res) => {
+    const linkedinSocial = {
+        userName: "Pratik Chakraborty",
+        followers: 2500,
+        following: 201,
+        posts: 13,
+        date: new Date()
+    };
+    res.status(200).json(linkedinSocial)
+    
+});
+
+app.get("/api/v1/:token", (req, res) => {
+    console.log(req.params.token)
+    res.status(200).json({param: req.params.token,
+    error: "Requested data not available"})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
